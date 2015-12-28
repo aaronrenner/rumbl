343b865667aab6463f5e167f4b65e526452b3ca3
Aaron Renner <aaron@animascodelabs.com>
2015-12-28 15:42:04 -0700

    Added /watch infrastructure

--- web/router.ex
+++ web/router.ex
@@ -20,6 +20,7 @@ defmodule Rumbl.Router do
     get "/", PageController, :index
     resources "/sessions", SessionController, only: [:new, :create, :delete]
     resources "/users", UserController, only: [:index, :show, :new, :create]
+    get "/watch/:id", WatchController, :show
   end
 
   scope "/manage", Rumbl do
