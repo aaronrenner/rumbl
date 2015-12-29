8ef317bdef13f9844414861c49c96136ff10338f
Aaron Renner <aaron@animascodelabs.com>
2015-12-28 19:24:36 -0700

    Initial socket implementation

---
+++ web/static/js/video.js
@@ -0,0 +1,47 @@
+import Player from "./player"
+
+let Video = {
+  init(socket, element) {
+    if(!element) { return }
+
+    let msgContainer = document.getElementById("msg-container");
+    let msgInput = document.getElementById("msg-input");
+    let postButton = document.getElementById("msg-submit");
+    let videoId = element.getAttribute("data-id");
+    let playerId = element.getAttribute("data-player-id");
+
+    Player.init(element.id, playerId);
+
+    socket.connect()
+
+    let vidChannel = socket.channel("videos:" + videoId);
+
+
+    postButton.addEventListener("click", e => {
+      let payload = {body: msgInput.value, at: Player.getCurrentTime()};
+      vidChannel.push("new_annotation", payload)
+                .receive("error", e => console.log(e));
+      msgInput.value = ""
+    });
+
+    vidChannel.on("new_annotation", (resp) => {
+      this.renderAnnotation(msgContainer, resp);
+    });
+
+    vidChannel.join()
+      .receive("ok", resp => console.log("joined the video channel", resp))
+      .receive("ok", reason => console.log("join failed", reason))
+  },
+
+  renderAnnotation(msgContainer, {user, body, at}){
+    let template = document.createElement("div");
+    template.innerHTML = `
+      <b>${user.username}</b>: ${body}
+    `;
+
+    msgContainer.appendChild(template);
+    msgContainer.scrollTop = msgContainer.scrollHeight;
+  }
+};
+
+export default Video;
