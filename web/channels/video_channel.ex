c46ea3c093f04d298ec57b55964c08c08193d493
Aaron Renner <aaron@animascodelabs.com>
2015-12-29 14:37:13 -0700

    Trigger a new InfoSys task when an annotation is added

--- web/channels/video_channel.ex
+++ web/channels/video_channel.ex
@@ -27,15 +27,37 @@ defmodule Rumbl.VideoChannel do
       |> Rumbl.Annotation.changeset(params)
 
     case Repo.insert(changeset) do
-      {:ok, annotation} ->
-        broadcast! socket, "new_annotation", %{
-          user: Rumbl.UserView.render("user.json", %{user: user}),
-          body: annotation.body,
-          at: annotation.at
-        }
+      {:ok, ann} ->
+        broadcast_annotation(socket, ann)
+        Task.start_link(fn -> compute_additional_info(ann, socket) end)
         {:reply, :ok, socket}
       {:error, changeset} ->
         {:reply, {:error, %{errors: changeset}}, socket}
+    end
+  end
+
+  defp broadcast_annotation(socket, annotation) do
+    annotation = Repo.preload(annotation, :user)
+    rendered_ann = Phoenix.View.render(Rumbl.AnnotationView, "annotation.json", %{
+      annotation: annotation
+    })
+
+    broadcast! socket, "new_annotation", rendered_ann
+  end
+
+  def compute_additional_info(ann, socket) do
+    for result <- Rumbl.InfoSys.compute(ann.body, limit: 1) do
+      attrs = %{url: result.url, body: result.text, at: ann.at}
+
+      info_changeset =
+        Repo.get_by!(Rumbl.User, username: result.backend)
+        |> build_assoc(:annotations, video_id: ann.video_id)
+        |> Rumbl.Annotation.changeset(attrs)
+
+      case Repo.insert(info_changeset) do
+        {:ok, info_ann} -> broadcast_annotation(socket, info_ann)
+        {:error, changeset} -> IO.inspect changeset
+      end
     end
   end
 end
